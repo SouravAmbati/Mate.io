e763395d9c7feb47fbd33cf0d3a096d118c6b8fa
Sourav <souravambati@gmail.com>
2025-09-27 11:02:15 +0100

    vercel cors update

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "*",
+  origin: "https://mate-io-frontend.vercel.app",
   credentials: true
 }));
 
