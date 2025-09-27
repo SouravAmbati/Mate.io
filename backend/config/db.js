ee40e46b9726db09763350bf64e36950453e1279
Sourav <souravambati@gmail.com>
2025-09-27 08:34:53 +0100

    database update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(`${process.env.MONGO_URI}/mateio`);
     console.log(`MongoDB connected ${process.env.MONGO_URI}`);
   } catch (error) {
     console.error("MongoDB connection failed ❌", error.message);
