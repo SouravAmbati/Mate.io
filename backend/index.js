58083b89ee14cf7a0124a754b0ec7ecfb86b2070
Sourav <souravambati@gmail.com>
2025-10-05 13:49:29 +0100

    re update

--- backend/index.js
+++ backend/index.js
@@ -17,34 +17,13 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins = [
-  "https://mate-io-frontend.vercel.app",
-  "https://mate-io-frontend-git-main-souravs-projects-65.vercel.app"
-];
 // app.use(cors({
 //   origin: "http://localhost:5173",
 //   credentials: true
 // }));
-// app.use(cors({
-//   // origin: "https://mate-io-frontend.vercel.app", // frontend URL
-//   origin: "mate-io-frontend-git-main-souravs-projects-65.vercel.app", // frontend URL
-//   credentials: true, // allow cookies
-// }));
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true); // allow non-browser requests
-    if(allowedOrigins.includes(origin)){
-      return callback(null, true);
-    } else {
-      return callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
-
-app.options("*", cors({
-  origin: allowedOrigins,
-  credentials: true,
+  origin: "https://mate-io-frontend.vercel.app", // frontend URL
+  credentials: true, // allow cookies
 }));
 
 
